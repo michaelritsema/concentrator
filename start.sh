bf1a291c8170a34a65b7586d6e8b4a49839eb8a3
Michael Ritsema <michael.ritsema@ziften.com>
2016-02-23 12:01:00 -0600

    adjusted default ports/settings to match demo server

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 #/usr/local/bin/gunicorn -w 1 -b :8000 --chdir=/opt/concentrator --env DJANGO_SETTINGS_MODULE=zjango.settings zjango.wsgi
-/usr/local/bin/python2.7 /opt/concentrator/manage.py runserver 0.0.0.0:8000  &> /dev/null &
-/usr/local/bin/python2.7 /opt/concentrator/manage.py splunk --max-rows=20 --service  &> /dev/null & 
+/usr/local/bin/python2.7 /opt/concentrator/manage.py runserver --settings zjango.local_settings 0.0.0.0:8001  &> /dev/null &
+/usr/local/bin/python2.7 /opt/concentrator/manage.py splunk --settings zjango.local_settings --max-rows=20 --service  &> /dev/null &
