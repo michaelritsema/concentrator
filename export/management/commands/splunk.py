29e3b32607c9eea47947b36b50f0f21a0b77fb13
Michael Ritsema <michael.ritsema@ziften.com>
2016-02-23 10:28:04 -0600

    gave splunk sync service option

--- export/management/commands/splunk.py
+++ export/management/commands/splunk.py
@@ -5,6 +5,8 @@ from django.core.management.base import BaseCommand, CommandError
 from datacollection.models import AgentMessage
 from export.models import ConsumerOffset
 from django.conf import settings
+from time import sleep
+
 
 class Command(BaseCommand):
     help = 'Sync data with splunk'
@@ -23,25 +25,14 @@ class Command(BaseCommand):
             default=5,
             help='Max rows to sync')
 
-
-    def handle(self, *args, **options):
-        if options['reset-offset']:
-            c = ConsumerOffset.objects.get(consumer='splunk')
-            c.last_row_id = 0
-            c.save()
-            self.stdout.write("Reseting consumer offset to 0.")
-            return
+        parser.add_argument('--service',
+            dest='max_rows',
+            default=5,
+            help='Max rows to sync')
 
 
-        #options['poll_id']:
-        consumer = ConsumerOffset.objects.get(consumer='splunk')
 
-        max_rows = 5
-
-        if 'max_rows' in options:
-            max_rows = options['max_rows']
-
-
+    def handle_loop(self, max_rows):
         rows = list(AgentMessage.objects.filter(id__gt=consumer.last_row_id).order_by('id')[:max_rows])
         row_count = len(rows)
         self.stdout.write("Sending data to splunk. Sending %s found rows after last_row_id %s" % (row_count, consumer.last_row_id))
@@ -55,6 +46,35 @@ class Command(BaseCommand):
             consumer.last_row_id = rows[-1].id
             consumer.save()
             self.stdout.write("Set last_row_id to %s" %(consumer.last_row_id,))
+
+    def handle(self, *args, **options):
+        if options['reset-offset']:
+            c = ConsumerOffset.objects.get(consumer='splunk')
+            c.last_row_id = 0
+            c.save()
+            self.stdout.write("Reseting consumer offset to 0.")
+            return
+
+        run_as_service = False
+
+        if options['service']:
+            run_as_service = True
+
+        #options['poll_id']:
+        consumer = ConsumerOffset.objects.get(consumer='splunk')
+
+        max_rows = 5
+
+        if 'max_rows' in options:
+            max_rows = options['max_rows']
+
+        if run_as_service:
+            while True:
+                self.handle_loop(max_rows)
+                sleep(5)
+
+        self.handle_loop(max_rows)
+
 
 
     def sync_splunk(self, log_data_list):
